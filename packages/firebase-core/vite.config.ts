2be67ab69475d3f751b8358b3ce023c12673d498
Luiz Braga <luiz.braga@lbdev.com.br>
2026-03-18 00:31:17 -0300

    Bump `vite` to 8.0.0 and replace `vite-tsconfig-paths` plugin with native `tsconfigPaths` support

--- packages/firebase-core/vite.config.ts
+++ packages/firebase-core/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { peerDependencies } from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   build: {
     minify: false,
     lib: {
@@ -21,7 +23,6 @@ export default defineConfig({
     },
   },
   plugins: [
-    tsconfigPaths(),
     dts({
       tsconfigPath: "../../tsconfig.app.json",
       rollupTypes: true,
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { peerDependencies } from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   build: {
     minify: false,
     lib: {
@@ -32,7 +34,6 @@ export default defineConfig({
     },
   },
   plugins: [
-    tsconfigPaths(),
     react(),
     dts({
       tsconfigPath: "./tsconfig.app.json",
